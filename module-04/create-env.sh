c553136e37ac883d29a689c26d8aa066ada71259
Lalitha Pammi <lalitha.pammi@live.com>
2024-06-06 20:41:29 -0700

    echoing target arn

--- module-04/create-env.sh
+++ module-04/create-env.sh
@@ -36,8 +36,10 @@ echo $SUBNET2B
 
 echo 'Creating the TARGET GROUP and storing the ARN in $TARGETARN...'
 # https://awscli.amazonaws.com/v2/documentation/api/2.0.34/reference/elbv2/create-target-group.html
-TARGETARN=$(aws elbv2 describe-target-groups --query 'TargetGroups[].[TargetGroupArn]' --names $8 --output text)
+TARGETARN=$(aws elbv2 create-target-group --name $8 --protocol HTTP --port 80 --target-type instance --vpc-id vpc-eaed8283 --output=text)
+echo $TARGETARN
 # aws elbv2 create-target-group --name $8 --protocol HTTP --port 80 --target-type instance --vpc-id vpc-eaed8283 --output=text
+#aws elbv2 describe-target-groups --query 'TargetGroups[].[TargetGroupArn]' --names $8 --output=text
 
 echo "Creating ELBv2 Elastic Load Balancer..."
 #https://awscli.amazonaws.com/v2/documentation/api/2.0.34/reference/elbv2/create-load-balancer.html
