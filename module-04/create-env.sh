4d499a3a40a8e7fe4efd0aa746e1d1e355aee8d0
Lalitha Pammi <lalitha.pammi@live.com>
2024-06-12 20:18:35 -0700

    adding json data

--- module-04/create-env.sh
+++ module-04/create-env.sh
@@ -59,7 +59,10 @@ echo $SUBNET2B
 echo "Creating the AutoScalingGroup Launch Template..."
 aws ec2 create-launch-template \
   --launch-template-name $12 \
-  --launch-template-data file://config.json \
+  --launch-template-data '{ "NetworkInterfaces": [ { "DeviceIndex": 0, "AssociatePublicIpAddress": true, "Groups": [ "sg-0f3e13389c52942b2" ], 
+    "SubnetId": "subnet-e9a0dd80", "DeleteOnTermination": true } ], "ImageId": "ami-09040d770ffe2224f", "InstanceType": "t2.micro", "KeyName": "coursera-key",
+    "UserData": "IyEvYmluL2Jhc2gKCiMgU2FtcGxlIGNvZGUgdG8gaW5zdGFsbCBOZ2lueCB3ZWJzZXJ2ZXIKCnN1ZG8gYXB0IHVwZGF0ZQpzdWRvIGFwdCBpbnN0YWxsIC15IG5naW54CgpzdWRvIHN5c3RlbWN0bCBlbmFibGUgLS1ub3cgbmdpbng=", "Placement": { "AvailabilityZone": "us-east-2a" },
+    "TagSpecifications":[{"ResourceType":"instance","Tags":[{"Key":"module","Value": "module3-tag" }]}] }'\
   --region $17
 echo "Launch Template created..."
 
