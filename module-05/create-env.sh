805121c60c1f28a247a707a1c9ca0f20dbd15ef2
Lalitha Pammi <lalitha.pammi@live.com>
2024-06-22 16:36:07 -0700

    fix error for autoscale

--- module-05/create-env.sh
+++ module-05/create-env.sh
@@ -94,7 +94,7 @@ echo 'Creating Auto Scaling Group...'
 # https://awscli.amazonaws.com/v2/documentation/api/latest/reference/autoscaling/create-auto-scaling-group.html
 aws autoscaling create-auto-scaling-group \
   --auto-scaling-group-name $13 \
-  --launch-template LaunchTemplateId=$LAUNCHTEMPLATEID, Version='$Latest' \
+  --launch-template LaunchTemplateId=$LAUNCHTEMPLATEID,Version='1' \
   --target-group-arns $TARGETARN \
   --health-check-type EC2 \
   --health-check-grace-period 600 \
